a2a5565f952bfbcd03c32be9412122377910ce1d
Silent <siltengen@gmail.com>
2021-11-08 15:20:24 +0100

    Changed for local usage

--- dl.py
+++ dl.py
@@ -1,16 +1,16 @@
-import sys, os
-from skillshare import Skillshare, splash
-from magic import cookie
+import sys
+import os
+from skillshare import Skillshare
 
 # or by class ID:
-# dl.download_course_by_class_id(189505397)
+# Skillshare.download_course_by_class_id(189505397)
+
 
 def main():
-    dl = Skillshare(cookie)
+    dl = Skillshare()
     course_url = sys.argv[1]
     dl.download_course_by_url(course_url)
 
 
 if __name__ == "__main__":
-    splash()
     main()
--- skillshare.py
+++ skillshare.py
@@ -1,12 +1,17 @@
-import requests, json, sys, re, os
+import requests
+import json
+import sys
+import re
+import os
 import cloudscraper
 from slugify import slugify
+
 
 class Skillshare(object):
     def __init__(
         self,
-        cookie,
-        download_path=os.environ.get('FILE_PATH', './Skillshare'),
+        cookie='\n"device_session_id=9488a019-fbdb-4b22-957b-83c244ccd69c; show-like-copy=0; YII_CSRF_TOKEN=aGFxOUtGOHRwYW9pb3FROEF0dEszbVZxeXJEb05PRWQSkeOgOjZCNMEufXqBephtlWYpi2WC1x2aPWb9Q5q2nA%3D%3D; visitor_tracking=utm_campaign%3D%26utm_source%3D%28direct%29%26utm_medium%3D%28none%29%26utm_term%3D%26referrer%3D%26referring_username%3D; first_landing=utm_campaign%3D%26utm_source%3D%28direct%29%26utm_medium%3D%28none%29%26utm_term%3D%26referrer%3D%26referring_username%3D; G_ENABLED_IDPS=google; __stripe_mid=9b2b4cb8-7eaa-4164-95d5-7ce103175a9cf456f9; __cf_bm=7e50f0077f1ce561a736e69c4ff4fa4c468827a7-1630118745-1800-AfPfznKdAJd+hT3XcLcvo+DjBYrofiD1iQvI76SCJ/7xlUL02DF1kLrCiPY3taKED1JP9D+ueU3TquUMChpW9LYPhdJ6r/9m7vGMN35LtXSv; __stripe_sid=02c55bb1-6a9f-4553-a5da-d404cdda62adbd25d1; g_state={&quot;i_p&quot;:1630126023659,&quot;i_l&quot;:1}; PHPSESSID=0a2751ee95ce89ff26cf04bb9c65b762; skillshare_user_=0a603dcc4f926238be6c4387f45dbaf44f79e467a%3A4%3A%7Bi%3A0%3Bs%3A8%3A%2221109653%22%3Bi%3A1%3Bs%3A25%3A%22graceadesina552%40gmail.com%22%3Bi%3A2%3Bi%3A7776000%3Bi%3A3%3Ba%3A4%3A%7Bs%3A8%3A%22username%22%3Bs%3A9%3A%22289054965%22%3Bs%3A10%3A%22login_time%22%3Bs%3A19%3A%222021-08-28%2002%3A47%3A07%22%3Bs%3A10%3A%22touch_time%22%3Bs%3A19%3A%222021-08-28%2002%3A47%3A34%22%3Bs%3A5%3A%22roles%22%3Bs%3A7%3A%22premium%22%3B%7D%7D"\n',
+        download_path=os.path.join(os.path.dirname(__file__), "content"),
         pk='BCpkADawqM2OOcM6njnM7hf9EaK6lIFlqiXB0iWjqGWUQjU7R8965xUvIQNqdQbnDTLz0IAO7E6Ir2rIbXJtFdzrGtitoee0n1XXRliD-RH9A-svuvNW9qgo3Bh34HEZjXjG4Nml4iyz3KqF',
         brightcove_account_id=3695997568001,
     ):
@@ -103,11 +108,11 @@ class Skillshare(object):
         res = scraper.get(
             url,
             headers={
-            'Accept': 'application/vnd.skillshare.class+json;,version=0.8',
-            'User-Agent': 'Skillshare/5.3.0; Android 9.0.1',
-            'Host': 'api.skillshare.com',
-            'Referer': 'https://www.skillshare.com/',
-            'cookie': self.cookie,
+                'Accept': 'application/vnd.skillshare.class+json;,version=0.8',
+                'User-Agent': 'Skillshare/5.3.0; Android 9.0.1',
+                'Host': 'api.skillshare.com',
+                'Referer': 'https://www.skillshare.com/',
+                'cookie': self.cookie,
             }
         )
 
@@ -172,32 +177,8 @@ class Skillshare(object):
                     dl += len(data)
                     f.write(data)
                     done = int(50 * dl / total_length)
-                    sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50 - done)))
+                    sys.stdout.write("\r[%s%s]" %
+                                     ('=' * done, ' ' * (50 - done)))
                     sys.stdout.flush()
 
             print('')
-
-def splash():
-
-    print(r"""   
-                 ____  _    _ _ _     _                          ____  _     
-                / ___|| | _(_) | |___| |__   __ _ _ __ ___      |  _ \| |    
-                \___ \| |/ / | | / __| '_ \ / _` | '__/ _ \_____| | | | |    
-                 ___) |   <| | | \__ \ | | | (_| | | |  __/_____| |_| | |___ 
-                |____/|_|\_\_|_|_|___/_| |_|\__,_|_|  \___|     |____/|_____|  
-                             _ __ ___  _ _  _ _ _  ___  _ _ 
-                            | / /| __>| \ || | | || . || | |
-                            |  \ | _> |   || | | ||   |\   /
-                            |_\_\|___>|_\_||__/_/ |_|_| |_| 
-                    
-                    
-                        
-                     ####### #     # ####### #     #    #     #####  #    # 
-                     #     # ##    # #       #     #   # #   #     # #   #  
-                     #     # # #   # #       #     #  #   #  #       #  #   
-                     #     # #  #  # #####   ####### #     # #       ###    
-                     #     # #   # # #       #     # ####### #       #  #   
-                     #     # #    ## #       #     # #     # #     # #   #  
-                     ####### #     # ####### #     # #     #  #####  #    # 
-                                                                                                         
-                """)
